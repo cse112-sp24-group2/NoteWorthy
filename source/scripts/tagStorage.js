5e495dcedf48d2a9198be13d4fc9b131f3f8fe3a
Andy Liu <aandyliu46@gmail.com>
2024-05-05 13:40:19 -0700

    created file for tagStorage + initialize TagDB

---
+++ source/scripts/tagStorage.js
@@ -0,0 +1,33 @@
+let tagDB;
+
+/**
+ * Sets up and returns a reference to our IndexedDB tags storage.
+ * @returns A reference to our IndexedDB tags storage.
+ */
+export function initializeTagDB() {
+    return new Promise((resolve, reject) => {
+        if (tagDB) {
+          resolve(tagDB);
+          return;
+        }
+        // creates tag indexDB
+        const tagsDBopenReq = indexedDB.open('tags_DB', 2);
+        tagsDBopenReq.onupgradeneeded = (event) => {
+          // Set up the database table if it hasn't been initialized yet.
+          tagDB = event.target.result;
+          const objectStore = tagDB.createObjectStore('tags', {
+            keyPath: 'uuid',    
+            autoIncrement: true,
+          });
+        //   objectStore.createIndex('notes', 'notes', { unique: false });
+        };
+
+        tagsDBopenReq.onsuccess = (event) => {
+          tagDB = event.target.result;
+          resolve(tagDB);
+        };
+        tagsDBopenReq.onerror = (event) => {
+          reject(new Error(`Error opening tag database! ${event.target.errorCode}`));
+        };
+      });
+}

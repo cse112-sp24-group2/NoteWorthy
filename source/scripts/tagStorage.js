8c925abd90dccc32aef81a1793b691d9ff12540c
Andy Liu <aandyliu46@gmail.com>
2024-05-09 22:44:24 -0700

    default tags upon database creation

--- source/scripts/tagStorage.js
+++ source/scripts/tagStorage.js
@@ -1,41 +1,41 @@
 let tagDB;
 const TAG_STORE_NAME = 'tags';
+// const initialTags = [{ 1: "work" }, { 2: "projects" }, { 3: "personal" }];
 
 /**
  * Sets up and returns a reference to our IndexedDB tags storage.
  * @returns A reference to our IndexedDB tags storage.
  */
 export function initializeTagDB() {
-    return new Promise((resolve, reject) => {
-        if (tagDB) {
-          resolve(tagDB);   
-          return;
-        }
-        // creates tag indexDB
-        const tagsDBopenReq = indexedDB.open('tags_DB', 2);
-        tagsDBopenReq.onupgradeneeded = (event) => {
-          // Set up the database table if it hasn't been initialized yet.
-          tagDB = event.target.result;
-          const objectStore = tagDB.createObjectStore(TAG_STORE_NAME, {
-            keyPath: 'uuid',    
-            autoIncrement: true,
-          });
-        //   objectStore.createIndex('notes', 'notes', { unique: false });
-        // adding default tags into the database
-        objectStore.add("Work");
-        objectStore.add("Projects");
-        objectStore.add("Home");
-        objectStore.add("Goals");
-        };
-
-        tagsDBopenReq.onsuccess = (event) => {
-          tagDB = event.target.result;
-          resolve(tagDB);
-        };
-        tagsDBopenReq.onerror = (event) => {
-          reject(new Error(`Error opening tag database! ${event.target.errorCode}`));
-        };
+  return new Promise((resolve, reject) => {
+    if (tagDB) {
+      resolve(tagDB);
+      return;
+    }
+    // creates tag indexDB
+    const tagsDBopenReq = indexedDB.open('tags_DB');
+    tagsDBopenReq.onupgradeneeded = (event) => {
+      // Set up the database table if it hasn't been initialized yet.
+      tagDB = event.target.result;
+      const objectStore = tagDB.createObjectStore(TAG_STORE_NAME, {
+        keyPath: 'id',
+        autoIncrement: true,
       });
+      const initialTags = [{ 1: "work" }, { 2: "projects" }, { 3: "personal" }];
+      for (var i = 0; i < initialTags.length; i++) {
+        objectStore.put(initialTags[i]);
+      }
+
+    };
+
+    tagsDBopenReq.onsuccess = (event) => {
+      tagDB = event.target.result;
+      resolve(tagDB);
+    };
+    tagsDBopenReq.onerror = (event) => {
+      reject(new Error(`Error opening tag database! ${event.target.errorCode}`));
+    };
+  });
 }
 
 

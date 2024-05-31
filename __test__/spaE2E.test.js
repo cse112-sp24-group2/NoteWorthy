9659f8dee80e2c535aa363ab77759071bfef91df
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2024-05-30 20:55:00 -0700

    test run

---
+++ __test__/spaE2E.test.js
@@ -0,0 +1,35 @@
+import puppeteer from 'puppeteer';
+
+const URL = 'http://127.0.0.1:8080/source/index.html';
+
+let browser;
+let page;
+
+const beforebefore = async () => {
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  page.setDefaultTimeout(0);
+};
+
+const afterafter = async () => {
+  await page.close();
+  await browser.close();
+};
+
+/**
+ * Tests:
+ * - Load 3D homepage
+ * - Load 2D homepage
+ */
+describe('Load Tests', () => {
+  beforeEach(beforebefore);
+
+  it('Loads 2D homepage', async () => {
+    await page.goto(URL)
+
+    const newNote = await page.$('#newNote');
+    await newNote.click();
+  }, 10000);
+
+  afterEach(afterafter);
+}, 30000);

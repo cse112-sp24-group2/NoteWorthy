1a70525966a086d6e8be3c43a78a2f59b740d88a
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2024-06-04 11:35:55 -0700

    lighthouse config fix

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,10 +8,10 @@ module.exports = {
     },
     assertions: {
       // Set the minimum score thresholds here
-      'categories:performance': ['error', { minScore: 0.9 }],
-      'categories:accessibility': ['error', { minScore: 0.9 }],
-      'categories:best-practices': ['error', { minScore: 0.9 }],
-      'categories:seo': ['error', { minScore: 0.9 }],
+      'categories:performance': ['error', { "minScore": 0.9 }],
+      'categories:accessibility': ['error', { "minScore": 0.9 }],
+      'categories:best-practices': ['error', { "minScore": 0.9 }],
+      'categories:seo': ['error', { "minScore": 0.9 }],
     },
   },
 };

e8419a0bfb66f6cc19a1656900b3ac03ecc528eb
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2024-06-04 12:08:20 -0700

    lighthouse assertions fix v10

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,15 +3,13 @@ module.exports = {
     collect: {
       staticDistDir: './source/',
     },
-    assertions: {
-      // Set the minimum score thresholds here
-      // 'categories:performance': ['error', { "minScore": 0.9 }],
-      // 'categories:accessibility': ['error', { "minScore": 0.9 }],
-      // 'categories:best-practices': ['error', { "minScore": 0.9 }],
-      // 'categories:seo': ['error', { "minScore": 0.9 }],
-      "audit-id-1": ["error", {"minScore": 0.9}],
-      "audit-id-2": ["error", {"minScore": 0.9}],
-      "audit-id-3": ["error", {"minScore": 0.9}],
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
     },
     upload: {
       target: 'temporary-public-storage',

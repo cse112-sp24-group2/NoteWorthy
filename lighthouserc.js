22eab40aba926a7a3141556c044bde24ee8da62b
MarcBaeuerle <MarcBaeuerle@users.noreply.github.com>
2024-06-04 19:36:56 +0000

    deploy: 965248f534f809f0618952bd049fd04b60f7e60f

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,16 +2,15 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './source/',
+      psiStrategy: 'desktop',
     },
-    assertions: {
-      // Set the minimum score thresholds here
-      // 'categories:performance': ['error', { "minScore": 0.9 }],
-      // 'categories:accessibility': ['error', { "minScore": 0.9 }],
-      // 'categories:best-practices': ['error', { "minScore": 0.9 }],
-      // 'categories:seo': ['error', { "minScore": 0.9 }],
-      "audit-id-1": ["error", {"minScore": 0.9}],
-      "audit-id-2": ["error", {"minScore": 0.9}],
-      "audit-id-3": ["error", {"minScore": 0.9}],
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
     },
     upload: {
       target: 'temporary-public-storage',

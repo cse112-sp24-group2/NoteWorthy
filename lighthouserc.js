de16f949389dc3cb44b7f3ff48cb345b25361082
MarcBaeuerle <MarcBaeuerle@users.noreply.github.com>
2024-06-04 18:34:37 +0000

    deploy: d3c78064c9821f104b2fe3718c428d73bf336487

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,5 +6,12 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    assertions: {
+      // Set the minimum score thresholds here
+      'categories:performance': ['error', { minScore: 0.9 }],
+      'categories:accessibility': ['error', { minScore: 0.9 }],
+      'categories:best-practices': ['error', { minScore: 0.9 }],
+      'categories:seo': ['error', { minScore: 0.9 }],
+    },
   },
 };

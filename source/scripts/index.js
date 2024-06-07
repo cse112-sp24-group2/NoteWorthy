2b32ed682eb18dcd01774e0f21bbba58a35bdc2a
Andy Liu <aandyliu46@gmail.com>
2024-06-06 23:59:31 -0700

    clearing tag input when leaving note

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -28,6 +28,7 @@ async function switchToDashboard(dom) {
   const notes = await getNotesFromStorage(db);
   const noteTagsElement = document.getElementById('notes-tags');
   noteTagsElement.innerHTML = '';
+  document.getElementById('tag-input').value = document.getElementById("tag-input").defaultValue;
   addNotesToDocument(notes);
   dom.editor.classList.add('hidden');
   dom.dashboard.classList.remove('hidden');
@@ -89,6 +90,7 @@ function URLRoutingHandler() {
 
   if (id == null) {
     switchToDashboard(dom);
+    document.getElementById('tag-input').value = document.getElementById("tag-input").defaultValue;
     // const tags = document.getElementById('notes-tags');
     // tags.remove();
     // document.getElementById('notes-tags').innerHTML = '';
